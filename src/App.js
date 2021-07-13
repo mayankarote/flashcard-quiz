c567727cdbc95d2eb9df59ae0be2d29fcee0df8a
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-07-13 11:37:06 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FlashcardList from "./FlashcardList";
 import "./app.css";
+import axios from "axios";
 
 export default function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setFlashcards(
+        res.data.results.map((questionItem, index) => {
+          const answer = questionItem.correct_answer;
+          const options = [...questionItem.incorrect_answers, answer];
+          return {
+            id: `${index} - ${Date.now()}`,
+            questions: questionItem.question,
+            answer: answer,
+            options: options.sort(() => Math.random() - 5),
+          };
+        })
+      );
+      console.log(res.data);
+    });
+  });
 
   return <FlashcardList flashcards={flashcards} />;
 }
