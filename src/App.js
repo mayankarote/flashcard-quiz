39a6ebc093af7c733d2e4a94b159f2141efa783f
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-07-13 10:47:52 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import FlashcardList from "./FlashcardList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  return <FlashcardList flashcards={flashcards} />;
 }
 
-export default App;
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "what is 2 + 2?",
+    answer: 4,
+    options: ["2", "3", "4", "5"],
+  },
+
+  {
+    id: 2,
+    question: "what is 5 + 2?",
+    answer: 7,
+    options: ["7", "3", "4", "5"],
+  },
+];
